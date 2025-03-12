52ddd97d87a674593be0fd5bbbecf5da1ce010ac
AHA-amin <mdalaminhossain573190@gmail.com>
2025-03-12 20:24:07 +0600

    Initial commit

---
+++ src/componants/Banner/Banner.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Banner() {
+    return (
+        <div className='max-w-7xl mx-auto border'>
+          <div>
+          <h1>Your Fitness Journey     </h1>
+          </div>
+          <div>
+          <h1>Starts Here</h1>
+          </div>
+
+        </div>
+    )
+}
+
+export default Banner
---
+++ src/componants/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navber from '../Navber/Navber'
+import Banner from '../Banner/Banner'
+
+function Home() {
+  return (
+    <div className='max-w-7xl mx-auto'>
+
+        <Navber></Navber>
+        <Banner></Banner>
+
+    </div>
+  )
+}
+
+export default Home
